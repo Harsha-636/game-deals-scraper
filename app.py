be8fc59407a8a80cc9c5b39839ff375088f9ff96
Harsha-636 <harshaavula542@gmail.com>
2026-06-21 15:15:37 +0530

    Update app.run to use environment PORT variable

--- app.py
+++ app.py
@@ -61,6 +61,6 @@ def get_stats():
         'best_deal': best_deal,
         'cheapest': cheapest
     })
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
